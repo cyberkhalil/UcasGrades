9d2177b6fa392056594716d025be46329bc62bd0
mahmoud khalil <khalil2535@protonmail.com>
2019-06-23 21:05:18 +0300

    Update netbeans project license & add license notices in each class
    Signed-off-by: mahmoud khalil <khalil2535@protonmail.com>

--- src/ciphers/Encryption.java
+++ src/ciphers/Encryption.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright (C) 2019 khalil2535
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package ciphers;
 
 /**
  * A Java implementation of Caesar Cipher. /It is a type of substitution cipher
- * in which each letter in the plaintext is replaced by a letter some fixed
+ * in which each letter in the plain text is replaced by a letter some fixed
  * number of positions down the alphabet. /
  *
  * @author khalil2535
